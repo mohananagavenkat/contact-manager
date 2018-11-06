ae0cbd407d92b41941fa30338b5ddbea2e01ac82
mohananagavenkat <mohananagavenkat@gmail.com>
2018-11-06 22:43:51 +0530

    created data folder which contains some fake data to simulate the rest apis response and as well added dummy contacts data

---
+++ src/app/data/contacts.data.ts
@@ -0,0 +1,41 @@
+import { Contact } from "../interfaces/contact";
+
+export const contactsData: Contact[] = [
+  {
+    firstName: "Mohana Naga Venkat",
+    lastName: "Sayempu",
+    phoneNumber: "9949270864",
+    email: "mohananagavenkat@gmail.com",
+    company: "Smactechlabs",
+    job: "web developer",
+    notes: "nothing",
+    avatar: "/assets/img/profilepic.jpg"
+  },
+  {
+    firstName: "Hemasri Sravya",
+    lastName: "Sayempu",
+    phoneNumber: "9631457822",
+    email: "sravya@gmail.com"
+  },
+  {
+    firstName: "Ratnamala",
+    lastName: "Nallapaneni",
+    phoneNumber: "9908439725",
+    email: "ratnamala.nallapaneni@gmail.com"
+  },
+  {
+    firstName: "Sivaram prasad",
+    lastName: "Sayempu",
+    phoneNumber: "9603531248",
+    email: "sivaramprasad553@gmail.com"
+  },
+  {
+    firstName: "Ammamma",
+    phoneNumber: "9177326204"
+  },
+  {
+    firstName: "Bava",
+    phoneNumber: "9989992566",
+    email: "bharathkumar.pamulapati@gmail.com"
+  }
+];
