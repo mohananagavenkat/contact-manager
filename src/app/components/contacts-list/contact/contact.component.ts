06e2c84e78858b9c39d1e4ce91f503e9b3f749c6
mohananagavenkat <mohananagavenkat@gmail.com>
2018-11-11 00:12:01 +0530

    created contact component which is responsible for contact ui

---
+++ src/app/components/contacts-list/contact/contact.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input, OnChanges } from "@angular/core";
+import { Contact } from "../../../interfaces/contact";
+import { ColorService } from "../../../services/color.service";
+
+@Component({
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["../contacts-list.component.css"]
+})
+export class ContactComponent implements OnInit, OnChanges {
+  @Input("contact")
+  contact: Contact;
+
+  @Input("canDisplay")
+  canDisplay;
+
+  background: string;
+
+  constructor(private colorService: ColorService) {}
+
+  ngOnInit() {}
+
+  ngOnChanges() {
+    console.log(this.contact.avatar);
+    console.log(this.colorService);
+    if (!this.contact.avatar) {
+      this.background = this.colorService.getColor();
+    }
+    console.log(this.background);
+  }
+  onContactFocus(event) {
+    console.log("clicked");
+    const ele = document.querySelector(".each-contact.has-focus");
+    if (ele) {
+      ele.classList.remove("has-focus");
+    }
+    event.target.classList.add("has-focus");
+  }
+}
