f8461dd4b7ecb6b6e5a7cfdbe263f0452c550c05
mohananagavenkat <mohananagavenkat@gmail.com>
2018-11-22 00:33:33 +0530

    updated the auth service with new api routes (/user prefix) -  and added new method for requesting, resending of activation token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { appConfig } from "../config";
 
@@ -15,17 +15,26 @@ export class AuthService {
   signup(data): Observable<any> {
     console.log("sending signup request to the api");
     console.log(data);
-    return this.http.post<any>(
-      `${appConfig.apiDomain}/signup`,
-      data
+    return this.http.post(
+      `${appConfig.apiDomain}/user/signup`,
+      data,
+      {
+        headers: new HttpHeaders({
+          'Content-Type': "application/json"
+        })
+      }
     );
   }
 
   signin(data): Observable<any> {
     return this.http.post<any>(
-      `${appConfig.apiDomain}/signup`,
+      `${appConfig.apiDomain}/user/signin`,
       data
     );
+  }
+
+  resendActivationToken(tokenId): Observable<any> {
+    return this.http.get<any>(`${appConfig.apiDomain}/user/resend/activationtoken/${tokenId}`)
   }
 
 }
