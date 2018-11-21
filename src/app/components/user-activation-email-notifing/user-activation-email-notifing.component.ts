e286c94f4a7b256861668f8f7b62353909ade505
mohananagavenkat <mohananagavenkat@gmail.com>
2018-11-22 00:31:42 +0530

    completed the notifying part - taking a tokenId as input which helps to send request to resend activation token api

--- src/app/components/user-activation-email-notifing/user-activation-email-notifing.component.ts
+++ src/app/components/user-activation-email-notifing/user-activation-email-notifing.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-user-activation-email-notifing',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserActivationEmailNotifingComponent implements OnInit {
 
-  constructor() { }
+  @Input('tokenId') tokenId: number;
+  error: string;
+  success: string;
+
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    console.log(this.tokenId);
+  }
+
+  resendActivationToken() {
+    this.authService
+      .resendActivationToken(this.tokenId)
+      .subscribe((response) => {
+        console.log(response);
+        if (response.status == false) {
+          this.error = response.error;
+          return;
+        }
+        this.success = response.message;
+      })
   }
 
 }
