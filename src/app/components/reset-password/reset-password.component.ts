4e2ce90ff8bef0cdae789b5088fa77f1d081ac60
mohananagavenkat <mohananagavenkat@gmail.com>
2018-11-25 19:01:08 +0530

    completed reset password functionality - validating the token - if valid displaying the form else error message - form validations - changing the password

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-reset-password",
@@ -6,7 +9,64 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["../auth-forms.css"]
 })
 export class ResetPasswordComponent implements OnInit {
-  constructor() {}
+  isAllowedToResetPassword: boolean = false;
+  token: string;
+  error: string = '';
+  id: any;
+  resetPasswordForm: FormGroup;
+  resetPasswordStatus: boolean = false;
+  resetPasswordError: string;
+  resetPasswordSuccess: string;
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.resetPasswordForm = new FormGroup({
+      id: new FormControl(null),
+      password: new FormControl(null, { validators: [Validators.required] }),
+      confirmPassword: new FormControl(null, { validators: [Validators.required] })
+    });
+    this.token = this.route.snapshot.params.token;
+    this.checkForgotPasswordToken();
+  }
+
+  checkForgotPasswordToken() {
+    this
+      .authService
+      .checkForgotPasswordToken({ token: this.token })
+      .subscribe(
+        response => {
+          if (response.status == false) {
+            this.error = response.error;
+            return;
+          }
+          this.resetPasswordForm.patchValue({ id: response.id })
+          this.isAllowedToResetPassword = true;
+          return;
+        }
+      );
+  }
+
+  resetPassword() {
+    this
+      .authService
+      .resetPassword(this.resetPasswordForm.value)
+      .subscribe(
+        response => {
+          if (response.status == false) {
+            this.resetPasswordError = response.error;
+            return;
+          }
+          this.resetPasswordStatus = true;
+          this.resetPasswordSuccess = response.message;
+          setTimeout(() => {
+            this.router.navigate(['/', 'user', 'signin']);
+          }, 3000)
+        }
+      )
+  }
+
 }
