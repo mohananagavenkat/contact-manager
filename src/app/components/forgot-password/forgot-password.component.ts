a5a1d4cf42ee199b186d92bb1dd1a84933293488
mohananagavenkat <mohananagavenkat@gmail.com>
2018-11-25 18:59:01 +0530

    completed forgot password related stuff - submiting email to api to check whther account exist or not if exist sending email with a link to change password

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: "app-forgot-password",
@@ -6,7 +8,37 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["../auth-forms.css"]
 })
 export class ForgotPasswordComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+  forgotPasswordForm: FormGroup;
+  showForgotPasswordEmailNotification: boolean = false;
+  error: string;
+  tokenId: any;
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+    this.forgotPasswordForm = new FormGroup({
+      email: new FormControl(null, { validators: [Validators.required, Validators.email] })
+    });
+  }
+
+  forgotPassword() {
+    this
+      .authService
+      .forgotPassword(this.forgotPasswordForm.value)
+      .subscribe(
+        response => {
+          if (response.status == false) {
+            this.error = response.error;
+            return;
+          }
+          this.showForgotPasswordEmailNotification = true;
+          this.tokenId = response.tokenId;
+          return;
+        }
+      )
+  }
+
 }
