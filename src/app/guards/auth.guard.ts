3bc2d7819c3b1dad87ed12893ef54a23eba9e375
mohananagavenkat <mohananagavenkat@gmail.com>
2018-12-13 23:24:18 +0530

    implemented auth check gaurd which prevents un authenticated user to vist protected routes

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,8 +1,19 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
 import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+import { AuthService } from "../services/auth.service";
 
+@Injectable()
 export class AuthGuard implements CanActivate {
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {
+    }
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-        return true;
+        if (this.authService.getAuthStatus())
+            return true;
+        this.router.navigate(['/', 'user', 'login']);
+        return false;
     }
 }
